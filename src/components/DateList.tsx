faaa1136eed5524fc5db88758f00a097786114c1
Andreas Goodstein <24245735+andreasgoodstein@users.noreply.github.com>
2022-09-19 09:48:57 +0200

    add weekday and color weekends

--- src/components/DateList.tsx
+++ src/components/DateList.tsx
@@ -1,3 +1,5 @@
+import { getDayName, isWeekend, parseDate } from "../helpers/dateHelper";
+
 type DateListProps = {
   dateList?: string[];
   selectDate: (date: string) => void;
@@ -20,16 +22,25 @@ export const DateList = ({
     >
       {dateList.map((date) => (
         <li style={{ display: "inline-flex" }} key={date}>
-          <button
-            onFocus={() => selectDate(date)}
-            onMouseOver={() => selectDate(date)}
-            style={{
-              backgroundColor: date === selectedDate ? "grey" : "transparent",
-            }}
-            type="button"
-          >
-            {date.substring(4)}
-          </button>
+          <div style={{ display: "flex", flexFlow: "column" }}>
+            <span>{getDayName(parseDate(date))}</span>
+
+            <button
+              onFocus={() => selectDate(date)}
+              onMouseOver={() => selectDate(date)}
+              style={{
+                backgroundColor:
+                  date === selectedDate
+                    ? "grey"
+                    : isWeekend(parseDate(date))
+                    ? "lightgrey"
+                    : "transparent",
+              }}
+              type="button"
+            >
+              {date.substring(4)}
+            </button>
+          </div>
         </li>
       ))}
     </ul>
--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -1,5 +1,12 @@
-import { add, format } from "date-fns";
+import { add, format, parse } from "date-fns";
+
+export const addDays = (date: Date, days: number) => add(date, { days });
 
 export const formatDate = (date: Date) => format(date, "yyyyMMdd");
 
-export const addDays = (date: Date, days: number) => add(date, { days });
+export const getDayName = (date: Date) => format(date, "iii");
+
+export const isWeekend = (date: Date) => [6, 0].includes(date.getDay());
+
+export const parseDate = (dateString: string) =>
+  parse(dateString, "yyyyMMdd", new Date());
